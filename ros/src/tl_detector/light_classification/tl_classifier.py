7a3fa66d92553665efe4b690198ae8e795f9279b
Alkim <alkim.mailbox@gmail.com>
2019-02-26 22:14:29 +0400

    attempt to integrate model into ros

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,9 +1,18 @@
 from styx_msgs.msg import TrafficLight
 
+from keras.preprocessing import image
+from keras.models import load_model
+
+import numpy as np
+
+import rospy
+
 class TLClassifier(object):
     def __init__(self):
-        #TODO load classifier
-        pass
+
+        #Todo: move to config
+        dir = '/capstone/ros/src/tl_detector/light_classification/'
+        self.loaded_model = load_model(dir + 'traffic_light_classifier.h5')
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
@@ -15,5 +24,20 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        img = image.img_to_array(image)
+        img = np.expand_dims(img, axis=0)
+
+        prediction = self.loaded_model.predict(img)
+
+        light = TrafficLight.UNKNOWN
+        score_treshold = 0.8
+        if prediction[0] > score_treshold:
+            light = TrafficLight.GREEN
+        if prediction[1] > score_treshold:
+            light = TrafficLight.YELLOW
+        if prediction[2] > score_treshold:
+            light = TrafficLight.RED
+
+        rospy.logerr("Light predicted: {}".format(light))
+
+        return light
