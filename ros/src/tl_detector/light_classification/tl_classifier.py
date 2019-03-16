9a212310b496278a4f8ea4d49ade68d5d5f04f28
Alkim <alkim.mailbox@gmail.com>
2019-03-17 00:20:05 +0400

    attempt to integrate models

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -3,9 +3,76 @@ from styx_msgs.msg import TrafficLight
 from keras.preprocessing import image
 from keras.models import load_model
 
-import numpy as np
-
 import rospy
+
+import cv2
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+from PIL import Image
+
+
+from keras.models import Sequential
+from keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Dropout
+from keras.utils.np_utils import to_categorical
+from keras import losses, optimizers, regularizers
+from keras.models import load_model
+from keras.preprocessing import image
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: np.expand_dims(image, 0)})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.uint8)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
 
 class TLClassifier(object):
     def __init__(self):
@@ -14,7 +81,16 @@ class TLClassifier(object):
         dir = '/capstone/ros/src/tl_detector/light_classification/'
         self.loaded_model = load_model(dir + 'traffic_light_classifier.h5')
 
-    def get_classification(self, image):
+        PATH_TO_FROZEN_GRAPH = dir + 'frozen_inference_graph.pb'
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+
+    def get_classification(self, image_param):
         """Determines the color of the traffic light in the image
 
         Args:
@@ -24,20 +100,44 @@ class TLClassifier(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        img = image.img_to_array(image)
-        img = np.expand_dims(img, axis=0)
+        image_np = image.img_to_array(image_param)
+        image_np_expanded = np.expand_dims(image_np, axis=0)
+        output_dict = run_inference_for_single_image(image_np, self.detection_graph)
 
-        prediction = self.loaded_model.predict(img)
+        detections = zip(output_dict['detection_boxes'][:3], output_dict['detection_classes'][:3],
+                         output_dict['detection_scores'][:3])
+
+        rospy.logerr("Detections: {}".format(detections))
 
         light = TrafficLight.UNKNOWN
-        score_treshold = 0.8
-        if prediction[0] > score_treshold:
-            light = TrafficLight.GREEN
-        if prediction[1] > score_treshold:
-            light = TrafficLight.YELLOW
-        if prediction[2] > score_treshold:
-            light = TrafficLight.RED
+        width = image_np.shape[1]
+        height = image_np.shape[0]
+        for box, detected_class, score in detections:
 
-        rospy.logerr("Light predicted: {}".format(light))
+            if detected_class != 10:
+                continue
+
+            if score <= 0.5:
+                continue
+
+            left = int(box[1] * width)
+            right = int(box[3] * width)
+            top = int(box[0] * height)
+            bottom = int(box[2] * height)
+
+            crop = image_np[top:bottom, left:right]
+            crop = cv2.resize(crop, (32, 64))
+
+            predictions = self.loaded_model.predict(np.expand_dims(crop, axis=0))
+
+            score_treshold = 0.8
+            if predictions[0] > score_treshold:
+                light = TrafficLight.GREEN
+            if predictions[1] > score_treshold:
+                light = TrafficLight.YELLOW
+            if predictions[2] > score_treshold:
+                light = TrafficLight.RED
+    
+            rospy.logerr("Light predicted: {}".format(light))
 
         return light
